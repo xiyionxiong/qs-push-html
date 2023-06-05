09c69d2cb6ae2aad8c4e0bcc6bae92ce1b25002a
熊凯 <xiongkai@petalmail.com>
2023-06-05 17:52:06 +0800

    feat(core): edge移动端会显示全屏的图标

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import useContext from "../GlobalState";
 import Keyboard from "./Keyboard";
 import { ContentWrapper, MainWrapper, TopWrapper } from "./Main.style";
@@ -17,35 +17,40 @@ const MainLayout = ({}: Props) => {
 
   const [visible, setVisible] = useState(false);
 
+  const isMobileEdge = useMemo(
+    () =>
+      navigator.userAgent.includes("EdgA") &&
+      navigator.userAgent.includes("Mobile"),
+    []
+  );
   return (
     <MainWrapper id="app-pay">
       <TopWrapper>
         <div className="left">
           <div className="title">
             Pay to Wiseasy{" "}
-            <svg
-              onClick={() => setVisible(true)}
-              style={{ marginLeft: 12 }}
-              viewBox="0 0 1024 1024"
-              version="1.1"
-              xmlns="http://www.w3.org/2000/svg"
-              p-id="21406"
-              width="16"
-              height="16"
-            >
-              <path
-                d="M923.905 338.957c-23.457-54.238-55.37-101.537-95.759-141.871-40.373-40.346-87.577-72.236-141.593-95.676-54.022-23.446-111.859-35.166-173.511-35.166-61.657 0-119.496 11.721-173.516 35.166-54.016 23.44-101.208 55.329-141.593 95.676-40.385 40.335-72.297 87.627-95.766 141.871-23.463 54.245-35.188 112.159-35.188 173.767 0 61.602 11.725 119.378 35.188 173.343 23.467 53.977 55.381 101.13 95.759 141.475 40.385 40.335 87.577 72.225 141.592 95.676 54.021 23.435 111.859 35.16 173.516 35.16 61.653 0 119.489-11.721 173.511-35.16 54.016-23.451 101.218-55.342 141.592-95.676 40.38-40.346 72.303-87.499 95.759-141.475 23.474-53.965 35.199-111.747 35.199-173.343 0.011-61.614-11.721-119.523-35.193-173.767l0 0zM513.042 800.443c-27.891 0-50.495-22.582-50.495-50.451 0-27.857 22.61-50.451 50.495-50.451 27.896 0 50.49 22.593 50.49 50.451 0 27.874-22.598 50.451-50.49 50.451l0 0zM563.532 590.709c0 27.863-22.598 50.462-50.49 50.462-27.891 0-50.495-22.598-50.495-50.462l0-316.084c0-27.874 22.61-50.462 50.495-50.462 27.896 0 50.49 22.588 50.49 50.462l0 316.084zM563.532 590.709z"
-                fill="#272636"
-                p-id="21407"
-              ></path>
-            </svg>
+            {isMobileEdge && (
+              <svg
+                style={{ marginLeft: 12 }}
+                onClick={() => screen.requestFullScreen("#app-pay")}
+                viewBox="0 0 1024 1024"
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                p-id="22435"
+                width="16"
+                height="16"
+              >
+                <path
+                  d="M213.333333 213.333333 426.666667 213.333333 426.666667 298.666667 298.666667 298.666667 298.666667 426.666667 213.333333 426.666667 213.333333 213.333333M597.333333 213.333333 810.666667 213.333333 810.666667 426.666667 725.333333 426.666667 725.333333 298.666667 597.333333 298.666667 597.333333 213.333333M725.333333 597.333333 810.666667 597.333333 810.666667 810.666667 597.333333 810.666667 597.333333 725.333333 725.333333 725.333333 725.333333 597.333333M426.666667 725.333333 426.666667 810.666667 213.333333 810.666667 213.333333 597.333333 298.666667 597.333333 298.666667 725.333333 426.666667 725.333333Z"
+                  p-id="22436"
+                ></path>
+              </svg>
+            )}
           </div>
           <div className="order">Order ID: {orderNo}</div>
         </div>
 
         <img
-          onDoubleClick={() => screen.exitFullscreen("#app-pay")}
-          onClick={() => screen.requestFullScreen("#app-pay")}
           src="https://wisecloud3-oss-dev.oss-cn-beijing.aliyuncs.com/markdown/wisecloud_logo_u&d_s.png"
           className="icons"
           alt="Vite logo"
